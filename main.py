1ae68e6a53370031a60ca580c0175680c0f17976
andrew hulme <andrew_hulme04@hotmail.com>
2024-03-16 22:06:39 +1100

    Miles to Kms Conversion working

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import tkinter
 
+def miles_to_kms():
+    miles = float(input.get())
+    km = miles * 1.609
+    result_label.config(text=f"{km}")
+
 window = tkinter.Tk()
-window.title("Miles to Kilometers")
-window.minsize(width=500, height=300)
+window.title("Mile to Km Converter")
+window.minsize(width=200, height=100)
+window.config(padx=30, pady=30)
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24))
-my_label.pack()
 
-def button_clicked():
-    my_label["text"] = input.get()
+miles_label = tkinter.Label(text="Miles")
+miles_label.grid(column=2, row=0)
 
-button = tkinter.Button(text="Click Me", command=button_clicked)
-button.pack()
+km_label = tkinter.Label(text="Kms")
+km_label.grid(column=2, row=1)
+
+button = tkinter.Button(text="Calculate", command=miles_to_kms)
+button.grid(column=1, row=2)
 
 input = tkinter.Entry()
-input.pack()
+input.grid(column=1, row=0)
+
+equals_label = tkinter.Label(text="Equals:")
+equals_label.grid(column=0, row=1)
+
+result_label = tkinter.Label(text="0")
+result_label.grid(column=1, row=1)
 
 window.mainloop()
